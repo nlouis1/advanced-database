69fe0d086eb13328099d780580b95879cae75bcc
Greg DeLozier <gregdelozier@users.noreply.github.com>
2025-08-27 18:39:36 +0000

    Updates to web application examples

--- topic-01-basic-web/app.py
+++ topic-01-basic-web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 #, render_template, request, redirect, url_for
 # import sqlite3
@@ -9,5 +9,29 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def get_index():
-    return "Hello from the flask server."
+    return render_template("index.html", item={
+        "name":"Greg",
+        "title":"Dr."
+    }, count=10)
 
+@app.route("/hi/<name>", methods=["GET"])
+@app.route("/hi", methods=["GET"])
+def get_hi(name="guest"):
+    return render_template("index.html", item={
+        "name":name
+    }, count=1)
+
+
+@app.route("/data", methods=["GET"])
+def get_data():
+    return {"data":[{
+        "name":"Greg",
+        "title":"Dr."
+
+    },
+    {
+        "name":"Maletic",
+        "title":"Professort."
+
+    }
+    ]}
